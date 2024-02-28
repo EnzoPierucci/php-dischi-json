ed6a5c12f2dc8e952801a72da732133d71c82a7a
EnzoPierucci <enzopierucci@gmail.com>
2024-02-28 14:54:34 +0100

    file

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js"></script>
+    <link href="https://cdn.tailwindcss.com" rel="stylesheet">
+    <title>php-dischi-json</title>
+    
+</head>
+<body>
+
+<script src="script.js"></script>
+<script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+</body>
+</html>
