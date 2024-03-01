5098cecc92c40685fcead23ae7c6988ca93987fc
EnzoPierucci <enzopierucci@gmail.com>
2024-03-01 15:14:58 +0100

    fix

--- getAlbum.php
+++ getAlbum.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 
 if (isset($_GET['title'])) {
-    $albums = json_decode(file_get_contents('dischi.json'), true);
+    $albums = json_decode(file_get_contents('data/dischi.json'), true);
     $selectedAlbum = array_filter($albums, function ($album) {
         return $album['title'] === $_GET['title'];
     });
